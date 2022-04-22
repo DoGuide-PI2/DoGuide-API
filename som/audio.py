136f741eeab7f73bf5fe10d4ad99852c9e44aff7
Mexazonic <brunnohenriq697@gmail.com>
2022-04-22 17:46:49 -0300

    Adição módulo de sonoplastia
    Co-authored-by: Eugenio Sales Siqueira <eugenio.sales@outlook.com>

---
+++ som/audio.py
@@ -0,0 +1,80 @@
+import sounddevice as sd
+from scipy.io.wavfile import write
+import wavio as wv
+import speech_recognition as sr
+from google_speech import Speech
+import keyboard
+import os
+from collections import defaultdict
+from tqdm import tqdm
+
+
+
+class ModuloSom(object):
+
+    def __init__(self):
+        self.dicionario_palavras= { 'eu':True, 'gostaria':True, 'de':True, 'ir':True, 'para':True,
+        'favor':True, 'ir':True, 'leve':True, 'me':True, 'mim':True, 'leve-me':True,
+        'desejo':True, 'por favor':True, 'por':True, 'quero':True, 'leva':True,
+        'daqui':True, 'onde':True, 'estou':True, 'pra':True, 'sair': True,
+        'o':True, 'a':True,'e':True,'i':True,'u':True, 'doguide':True, 'dogaide':True, 'dog':True, 'Dog': True
+        }
+
+        self.dicionario_positivo = {
+            'sim':True, 'ok':True, 'isso':True, 'confirmado':True, 'verdade':True, 'beleza':True, 'okay':True, 'yes':True, 'Sim': True
+        }
+
+        self.r = sr.Recognizer()
+        self.temp = 'temp/'        
+    def extract_local(self, recorded_voice):
+        local = []
+        for palavra in recorded_voice.split():
+            if(palavra.lower() not in self.dicionario_palavras):
+                local.append(palavra)    
+        return ' '.join(local)
+
+    def google_voice(self, frase):
+        lang = "pt"
+        speech = Speech(frase, lang)
+        speech.play()
+
+    def record(self, freq = 44100,  duration = 3):
+        recording = sd.rec(int(duration * freq), samplerate=freq, channels=2)
+        sd.wait()
+
+        return recording
+
+    def generate_audio(self, recording, freq=44100):
+        
+        if not os.path.exists(self.temp): os.makedirs(self.temp)
+
+        wv.write(self.temp + "temp_audio.wav", recording, freq, sampwidth=2)
+        load_audio = sr.AudioFile(self.temp + 'temp_audio.wav')
+        with load_audio as source:
+            data_audio = self.r.record(source)
+
+        return data_audio
+
+    def transcricao_audio(self, data_audio):
+        return self.r.recognize_google(data_audio, language = 'pt-PT')
+
+
+    def confirma_destino(self):
+        # google_voice('')
+        try:
+            print('Gravar Resposta')
+            recording = self.record(duration = 3)
+            data_audio = self.generate_audio(recording)
+            print('Resposta:', self.transcricao_audio(data_audio))
+            print('Gravação Encerrada')
+            
+            texto = self.r.recognize_google(data_audio, language = 'pt-PT')
+            for confirmacao in texto.lower().split():
+                if confirmacao in self.dicionario_positivo:
+                    # google_voice(texto)
+                    return True
+        except Exception as e:
+            print(e)
+            return False
+        
+
---
+++ som/utils.py
@@ -0,0 +1,73 @@
+import re
+import json
+
+
+def clean(string):
+    string = str(string)
+    string = re.sub(r"\n", ".", string)
+    string = re.sub(r"\r", "", string)
+    string = re.sub(r"\'", "", string)
+    string = re.sub(r"\"", "", string)
+    string = re.sub('[%s]' % re.escape("""!"#$%&'()*+,-./:;<=>?@[]^_`{|}~"""), ' ', string)
+    string = string.replace('\x0c', '')
+    string = string.replace('º', '')
+    string = string.replace('nº', '')
+    string = string.replace('II ', '2')
+    string = string.replace('Road', 'Avenida')
+    string = string.replace('QS', 'Quadra Sul')
+    string = string.replace('road', 'Avenida')
+    string = string.replace('Estrada de uso restrito', '')
+    string = string.replace('estrada de uso restrito', '')
+    string = string.replace('&nbsp', '')
+    string = string.replace('nbsp', '')
+
+
+    return string
+
+def cleanhtml(raw_html):
+    CLEANR = re.compile('<.*?>') 
+    cleantext = re.sub(CLEANR, ',', raw_html)
+    return cleantext
+
+
+def rota_json(local, rota):
+    with open(local.replace(' ', '_') + "_routes.json", 'w') as f:
+        f.write(json.dumps(rota, indent=4, ensure_ascii=False))
+
+
+def string_final_distancia(distancia, string):
+    if string.find('Vire') != -1:
+        return 'Em ' + distancia + ' ' + string
+    else:
+        return string + ' por ' + distancia 
+
+def string_distancia(distancia):
+
+    if (distancia.find('km') != -1):
+        distancia = distancia.replace(',', '.')
+        convert_distancia = re.findall("\d+\.\d+", distancia)
+        convert_distancia = float(*convert_distancia)
+        
+        distancia_final = str(str(convert_distancia) + ' quilometros' if convert_distancia >= 1 else ' quilometro') if convert_distancia >= 1 else (str(int(convert_distancia * 1000)) + ' metros')
+        # print(string_final_distancia(distancia_final))
+        return distancia_final
+
+    distancia_metros = re.findall("\d+", distancia)
+    # print(distancia_metros)
+    return str(int(*distancia_metros)) + ' metros'
+
+
+def instrucao_texto(rota):
+    trajetos = []
+    for instrucao in rota[0]['legs'][0]['steps']:
+        
+        distancia_final = string_distancia(instrucao['distance']['text'])
+        caminho = clean(cleanhtml(instrucao['html_instructions']))
+        instrucao_final = string_final_distancia(distancia_final, caminho)
+        print(instrucao_final)
+        trajetos.append(instrucao_final)
+    trajetos.append('Você chegou no seu destino!')
+
+    return trajetos
+
+
